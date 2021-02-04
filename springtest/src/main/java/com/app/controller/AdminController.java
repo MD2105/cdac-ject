2ea7eed9eedcfa66fdf7abc48bcc72957d325637
Manish Doble <manishdoble@manish21.local>
2021-02-04 14:16:48 +0530

    adding mailfunction

--- springtest/src/main/java/com/app/controller/AdminController.java
+++ springtest/src/main/java/com/app/controller/AdminController.java
@@ -1,8 +1,14 @@
 package com.app.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +30,8 @@ public class AdminController {
      public AdminController() {
     	 System.out.println("In Admin Controller");
      }
+     @Autowired
+ 	private JavaMailSender sender;
      @Autowired
   	private IAdmin admndao;
       @Autowired
@@ -48,13 +56,29 @@ public class AdminController {
      	return "/TrainerRegistration/register";
      }
      @GetMapping("/add")
-     public String add_user(@ModelAttribute("New_User") Trainer newUser,Model map) {
+     public String add_user(@ModelAttribute("New_User") Trainer newUser,Model map) throws UnsupportedEncodingException, MessagingException {
      	System.out.println("In adding user");
      	System.out.println(dao.add_User(newUser));
+     	sendMailTrainer(newUser);
      	map.addAttribute("msgt","You Have Signed up Successfully");
      	return "redirect:/user/login";
      }
-   
+     
+     public void sendMailTrainer(Trainer t) throws UnsupportedEncodingException, MessagingException {
+    	 String subject = "Login Details";
+    	 String senderName = "Assignment Evaluation System C-DAC";
+    	 String mailContent = "<p> Dear Trainer Id :"+t.getFaculty_id()+",</p>";
+    	 mailContent += "<p><b>Your Login Detail<b>s are Email : "+t.getFaculty_email()+" and Password : "+t.getFaculty_password()+"</p>";
+    	 mailContent+="<p>From,<br>Assignment Evaluation System C-DAC</p>";
+    	 MimeMessage msg = sender.createMimeMessage();
+    	 MimeMessageHelper help = new MimeMessageHelper(msg);
+    	 help.setFrom("onlineassignmentsystemcdac@gmail.com",senderName);
+    	 help.setTo(t.getFaculty_email());
+    	 help.setSubject(subject);
+    	 help.setText(mailContent, true);
+    	 sender.send(msg);
+     }
+     
      @RequestMapping("/studreg")
      public String Register_student(Model muser)
      {
@@ -64,12 +88,28 @@ public class AdminController {
      	return "/StudentRegistration/student-reg";
      }
      @GetMapping("/stuadd")
-     public String add_student(@ModelAttribute("New_User") Students newUser,Model map) {
+     public String add_student(@ModelAttribute("New_User") Students newUser,Model map) throws UnsupportedEncodingException, MessagingException {
      	System.out.println("In adding user");
      	System.out.println(dao.add_Student(newUser));
+     	sendMailStudent(newUser);
      	map.addAttribute("msgs","You Have Signed up Successfully");
      	return "redirect:/studentfile/login";
      }
+     public void sendMailStudent(Students s) throws UnsupportedEncodingException, MessagingException {
+    	 String subject = "Login Details";
+    	 String senderName = "Assignment Evaluation System C-DAC";
+    	 String mailContent = "<p> Dear Student PRN :"+s.getStudent_prn()+",</p>";
+    	 mailContent += "<p> <b> Your Login Details </b> are Email : "+s.getStudent_email()+" and Password : "+s.getStudent_password()+"</p>";
+    	 mailContent+="<p>From,<br>Assignment Evaluation System C-DAC</p>";
+    	 MimeMessage msg = sender.createMimeMessage();
+    	 MimeMessageHelper help = new MimeMessageHelper(msg);
+    	 help.setFrom("onlineassignmentsystemcdac@gmail.com",senderName);
+    	 help.setTo(s.getStudent_email());
+    	 help.setSubject(subject);
+    	 help.setText(mailContent, true);
+    	 sender.send(msg);
+     }
+     
      
      @RequestMapping("/course")
      public String course() {
